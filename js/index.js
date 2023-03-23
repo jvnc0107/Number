429ef52716ab89428d2f5eee4a7a88cbc04c19d6
joao victor <jvnc0107@gmail.com>
2023-03-23 12:15:42 -0300

    att

---
+++ js/index.js
@@ -0,0 +1,61 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
+let status = document.getElementById('status');
+let attempt = document.getElementById('attempt');
+let result = document.getElementById('result');
+
+const Guess = {
+    max: 10,
+    attemptsNumber:0,
+    numberDrawn: function randomValue(){
+        return Math.round(Math.random() * this.max);
+    }
+};
+
+let numberDrawn = Guess.numberDrawn();
+
+function updateAttempt(attempt, value){
+    attempt.innerHTML = 'tentativa: ' + value
+};
+
+function handleSubmit(e){
+    e.preventDefault();
+
+    let kick = document.getElementById('kick').value;
+
+    if(!kick){
+        alert('digite algum valor!')
+        return;
+    };
+    updateAttempt(attempt, ++Guess.attemptsNumber);
+
+    if(numberDrawn == kick){
+        playagain();
+        status.innerHTML = 'Parabéns, você acertou!';
+        result.style.transition = '0.4s';
+        result.style.backgroundColor = '#37c978';
+        result.style.color = '#fff';
+        status.style.color = '#fff';
+        clear();
+    } else if (numberDrawn > kick){
+        status.innerHTML = "o numero é maior!";
+        status.style.color= '#de4251';
+        clear();
+    } else if (numberDrawn < kick){
+        status.innerHTML = "o numero é menor!";
+        status.style.color = '#de4251';
+        clear();
+    }
+
+
+};
+function playagain (){
+    document.getElementById('btnrestart').style.display = 'flex';
+};
+function restart(){
+    document.location.reload(true);
+};
+function clear(){
+        document.getElementById('kick').value = '';     
+};
